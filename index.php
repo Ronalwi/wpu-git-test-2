d7345b84504fc7862a845bf78cb757abc9eac16b
Ronalwi <p21-ronal344@smkn7-smr-sch.id>
2023-07-18 12:09:46 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium impedit, sint esse soluta officiis odit tenetur, blanditiis natus similique voluptas doloremque deserunt maiores ad amet, modi iure tempore animi ea.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
